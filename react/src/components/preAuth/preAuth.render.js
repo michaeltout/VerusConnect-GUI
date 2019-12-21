74107818546e77f5bbcb7234b956959b09f7e2e5
Michael Toutonghi <toutonghi.michael@gmail.com>
2019-12-21 08:26:13 +0100

    Display changes

--- react/src/components/preAuth/preAuth.render.js
+++ react/src/components/preAuth/preAuth.render.js
@@ -21,7 +21,7 @@ export const PreAuthRender = function() {
     <div className="d-flex d-sm-flex d-md-flex align-items-center align-items-sm-center align-items-md-center contact-clean pre-auth-container">
       <div className="pre-auth-inner-container">
         <div className="col-lg-4 col-xl-6 offset-lg-4 offset-xl-3 text-center pre-auth-header-container">
-          <img src="assets/images/Verus-Logo.png" width="367px" height="103px"/>
+          <img src="assets/images/Verus-Logo.png" width="259px" height="134px"/>
           {/*<h1 className="pre-auth-logo-header">Verus</h1>*/}
         </div>
         {this.props.mainPathArray[1] ? COMPONENT_MAP[this.props.mainPathArray[1]] : null}
