bfce46ae12fc95b0f6dd22448cfff685c7f2824d
Michael Toutonghi <toutonghi.michael@gmail.com>
2020-01-14 01:36:34 +0100

    Add splash screen to cover screen for loading on app start

--- react/src/components/main/main.js
+++ react/src/components/main/main.js
@@ -4,18 +4,17 @@ import {
   setMainNavigationPath,
   loginUser,
   initConfig,
-  setModalNavigationPath,
   initStaticSystemData
 } from '../../actions/actionCreators';
 import { refreshSystemIntervals } from '../../actions/actionDispatchers'
 import mainWindow, { staticVar } from '../../util/mainWindow';
 import { POST_AUTH, PRE_AUTH, SELECT_PROFILE, ERROR_SNACK, MID_LENGTH_ALERT, NATIVE, UNLOCK_PROFILE } from '../../util/constants/componentConstants';
-import { getCoinObj } from '../../util/coinData';
 import Config from '../../config';
 import { connect } from 'react-redux';
 import PostAuth from '../postAuth/postAuth'
 import PreAuth from '../preAuth/preAuth'
 import Modal from '../modals/modal'
+import SplashScreen from '../../containers/SplashScreen/SplashScreen'
 import SnackbarAlert from '../snackbarAlert/snackbarAlert'
 import { newSnackbar, setConfigParams } from '../../actions/actionCreators'
 
@@ -24,58 +23,66 @@ import Store from '../../store'
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      initializing: false
+    }
   }
 
   componentDidMount() {
     const appVersion = mainWindow.appBasicInfo;
     const { dispatch } = this.props
 
-    // Initialize system data intervals and clear any old ones
-    refreshSystemIntervals()
+    this.setState({ initializing: true }, () => {
+      // Initialize system data intervals and clear any old ones
+      refreshSystemIntervals()
 
-    // Load users and config from file, system data from systeminformation lib
-    Promise.all([initUsers(), initConfig(), initStaticSystemData()])
-    .then(async (actionArray) => {
-      const userAction = actionArray[0]
-      const configActionArr = actionArray[1]
-      const staticSystemDataAction = actionArray[2]
-      
-      // Dispatch users, config, and system info to store
-      dispatch(userAction)
-      dispatch(staticSystemDataAction)
-      configActionArr.map(configAction => {
-        dispatch(configAction)
+      // Load users and config from file, system data from systeminformation lib
+      Promise.all([initUsers(), initConfig(), initStaticSystemData()])
+      .then(async (actionArray) => {
+        const userAction = actionArray[0]
+        const configActionArr = actionArray[1]
+        const staticSystemDataAction = actionArray[2]
+        
+        // Dispatch users, config, and system info to store
+        dispatch(userAction)
+        dispatch(staticSystemDataAction)
+        configActionArr.map(configAction => {
+          dispatch(configAction)
+        })
+
+        const { loadedUsers, config } = this.props
+        const { defaultUserId } = config.general.main
+
+        // Setup initial navigation state
+        if (defaultUserId && defaultUserId.length > 0 && loadedUsers[defaultUserId]) {
+          if (Object.values(loadedUsers[defaultUserId].startCoins).every((coinObj) => {
+            return coinObj.mode === NATIVE
+          }) || loadedUsers[defaultUserId].pinFile == null) {
+            loginUser(loadedUsers[defaultUserId]).map((action) => {
+              dispatch(action)
+            })
+          } else {
+            dispatch(setMainNavigationPath(`${PRE_AUTH}/${UNLOCK_PROFILE}`))
+          }
+        } else if (defaultUserId && defaultUserId.length > 0) {
+          try {
+            dispatch(await setConfigParams(config, { defaultUserId: '' }, 'general.main'))
+
+          } catch (e) {
+            dispatch(newSnackbar(ERROR_SNACK, e.message))
+            console.error(e)
+          }
+        } else if (Object.keys(loadedUsers).length > 0) {
+          dispatch(setMainNavigationPath(`${PRE_AUTH}/${SELECT_PROFILE}`))
+        }
+
+        this.setState({ initializing: false })
       })
-
-      const { loadedUsers, config } = this.props
-      const { defaultUserId } = config.general.main
-
-      // Setup initial navigation state
-      if (defaultUserId && defaultUserId.length > 0 && loadedUsers[defaultUserId]) {
-        if (Object.values(loadedUsers[defaultUserId].startCoins).every((coinObj) => {
-          return coinObj.mode === NATIVE
-        }) || loadedUsers[defaultUserId].pinFile == null) {
-          loginUser(loadedUsers[defaultUserId]).map((action) => {
-            dispatch(action)
-          })
-        } else {
-          dispatch(setMainNavigationPath(`${PRE_AUTH}/${UNLOCK_PROFILE}`))
-        }
-      } else if (defaultUserId && defaultUserId.length > 0) {
-        try {
-          dispatch(await setConfigParams(config, { defaultUserId: '' }, 'general.main'))
-
-        } catch (e) {
-          dispatch(newSnackbar(ERROR_SNACK, e.message))
-          console.error(e)
-        }
-      } else if (Object.keys(loadedUsers).length > 0) {
-        dispatch(setMainNavigationPath(`${PRE_AUTH}/${SELECT_PROFILE}`))
-      }
-    })
-    .catch(e => {
-      dispatch(newSnackbar(ERROR_SNACK, e.message, MID_LENGTH_ALERT))
-      console.error(e)
+      .catch(e => {
+        dispatch(newSnackbar(ERROR_SNACK, e.message, MID_LENGTH_ALERT))
+        console.error(e)
+        this.setState({ initializing: false })
+      })
     })
 
     if (appVersion) {
@@ -106,7 +113,7 @@ class Main extends React.Component {
         <div>
           <SnackbarAlert />
           <Modal />
-          {this.props.mainPathArray[0] === POST_AUTH ? <PostAuth /> : <PreAuth />}
+          {this.state.initializing ? <SplashScreen /> : (this.props.mainPathArray[0] === POST_AUTH ? <PostAuth /> : <PreAuth />)}
         </div>
       )
     } else {
