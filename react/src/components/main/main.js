13441a38b92d4746fb645ab39d306ae613cc8ce0
Michael Toutonghi <toutonghi.michael@gmail.com>
2020-07-12 00:51:02 +0200

    Comment out debug function

--- react/src/components/main/main.js
+++ react/src/components/main/main.js
@@ -173,9 +173,9 @@ class Main extends React.Component {
     }
 
     // Function for debugging store
-    window.printStore = () => {
+    /*window.printStore = () => {
       console.log(Store.getState());
-    };
+    };*/
   }
 
   render() {
