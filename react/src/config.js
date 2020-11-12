ee16d958cdf099f5aa2471a0f083809e1bdb2c78
michaeltout <toutonghi.michael@gmail.com>
2020-11-12 02:52:55 +0100

    Update API settings

--- react/src/config.js
+++ react/src/config.js
@@ -15,7 +15,7 @@ if (mainWindow) {
 }
 
 export const agamaPort = Config.general.main.agamaPort;
-export const apiEncryption = Config.general.main.encryptApi
+export const apiEncryption = true//Config.general.main.encryptApi
 export const secretToken = mainWindow.appSecretToken
 export const shieldKey = mainWindow.apiShieldKey
 export const rpc2cli = Config.general.native.rpc2cli;
