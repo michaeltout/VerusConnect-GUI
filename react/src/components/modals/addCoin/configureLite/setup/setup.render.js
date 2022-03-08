fabb95d88527cbaa2b1fb1c7642127ad4f646088
michaeltout <toutonghi.michael@gmail.com>
2022-03-09 00:31:29 +0100

    Rework seed import/creation UI for improved clarity

--- react/src/components/modals/addCoin/configureLite/setup/setup.render.js
+++ react/src/components/modals/addCoin/configureLite/setup/setup.render.js
@@ -3,8 +3,14 @@ import ProtectedInputForm from '../../../../../containers/ProtectedInputForm/Pro
 
 export const SetupRender = function() {
   return (
-    <div className="d-flex justify-content-center align-items-center flex-column" style={{ paddingBottom: 65 }}>
-      { this.state.seedGenerator ? SetupRenderSeedGenerator.call(this) : SetupRenderChoices.call(this) }
+    <div
+      className="d-flex align-items-center flex-column"
+      style={{
+        justifyContent: this.state.importSeed ? "center" : "space-between",
+        paddingBottom: this.state.importSeed ? 65 : 0,
+      }}
+    >
+      {this.state.importSeed ? SetupRenderSeedImport.call(this) : SetupRenderChoices.call(this)}
     </div>
   );
 }
@@ -12,58 +18,36 @@ export const SetupRender = function() {
 export const SetupRenderChoices = function() {
   return (
     <React.Fragment>
+      <div style={{height: '100%', display: "flex", justifyContent: "center"}}>
       <ProtectedInputForm 
-        heading="Enter an existing seed"
+        heading="Generate a new seed"
+        submitBtnText="Continue"
         multiline={true}
         onSubmit={this.submitSeed}
-        inlineSubmit={true}
+        confirmHeading="Confirm Seed"
+        confirm={true}
+        confirmBtnText="Confirm"
+        seedGenerator={true}
       />
-      <div
-        className="d-lg-flex flex-column justify-content-lg-center align-items-lg-center"
-      >
-        <h1
-          className="text-break text-center d-md-flex justify-content-md-center align-items-md-center"
-          style={{
-            marginBottom: 0,
-            color: "rgb(0,0,0)",
-            fontSize: 16,
-            width: "max-content",
-            paddingTop: 20,
-            paddingBottom: 40,
-            maxWidth: "100%"
-          }}
-        >
-          {"or"}
-        </h1>
       </div>
-      <button
-        className="btn btn-primary"
-        type="button"
-        style={{
-          fontSize: 14,
-          backgroundColor: "rgb(49, 101, 212)",
-          borderWidth: 1,
-          borderColor: "rgb(49, 101, 212)"
-        }}
+      <a
+        href="#"
+        style={{ color: "rgb(49, 101, 212)" }}
         onClick={ this.toggleSeedGenerator }
       >
-        {"Generate a New Seed"}
-      </button>
+        {"Import an existing secure seed phrase or WIF key"}
+      </a>
     </React.Fragment>
   )
 }
 
-export const SetupRenderSeedGenerator = function() {
+export const SetupRenderSeedImport = function() {
   return (
     <ProtectedInputForm 
-      heading="Generate a New Seed"
-      submitBtnText="Continue"
+      heading="Enter an existing secure seed phrase or WIF key"
       multiline={true}
       onSubmit={this.submitSeed}
-      confirmHeading="Confirm Seed"
-      confirm={true}
-      confirmBtnText="Confirm"
-      seedGenerator={true}
+      inlineSubmit={true}
     />
   )
 }
