4a99bcc102de2894d152a14ecdf1cb1d52a70169
michaeltout <toutonghi.michael@gmail.com>
2022-08-26 14:19:41 +0200

    Only include bridgekeeper info with mining info on VRSCTEST

--- react/src/util/api/wallet/readCalls/getMiningInfo.js
+++ react/src/util/api/wallet/readCalls/getMiningInfo.js
@@ -6,8 +6,8 @@ import { API_GET_MININGINFO } from '../../../constants/componentConstants'
  * @param {String} mode native
  * @param {String} chainTicker Ticker symbol for coin to fetch info for
  */
-export const getMiningInfo = async (mode, chainTicker) => {
-  const params = {chainTicker}
+export const getMiningInfo = async (mode, chainTicker, includeBridgekeeper = false) => {
+  const params = {chainTicker, includeBridgekeeper}
   let miningInfo = {}
 
   try {
